45d9cd3ab82ea39a8f1f0b0812870d9cb3f3bcd8
xtx <2656823393@qq.com>
2023-05-18 18:09:08 +0800

    fix: 修复多次配置时TCPCom的RecNewData被多次绑定的问题
    修复多次配置时TCPCom的RecNewData被多次绑定的问题

--- api/RepeaterWidget/RepeaterWidget.cpp
+++ api/RepeaterWidget/RepeaterWidget.cpp
@@ -27,6 +27,11 @@ void RepeaterWidget::SaveConstructConfig() {
 
 void RepeaterWidget::StopAllInfoTCP() {
     if (tcp_info_handler_[1] == nullptr) { return; }
+    if (tcp_info_handler_[2] == nullptr) { return; }
+    if (tcp_info_handler_[3] == nullptr) { return; }
+    disconnect(tcp_info_handler_[1], &TCPInfoHandle::RecNewData, 0, 0);
+    disconnect(tcp_info_handler_[2], &TCPInfoHandle::RecNewData, 0, 0);
+    disconnect(tcp_info_handler_[3], &TCPInfoHandle::RecNewData, 0, 0);
     if (tcp_info_handler_[1]->is_connected_) {
         tcp_info_handler_[1]->disconnectFromHost();
     }
